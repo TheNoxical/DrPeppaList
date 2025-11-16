f335fdb7b5d9101920d1dcdd63d3a70ea68f012a
TheNoxical <118859645+TheNoxical@users.noreply.github.com>
2025-11-16 01:22:42 -0500

    test test

--- js/score.js
+++ js/score.js
@@ -4,6 +4,12 @@
 const scale = 2;
 
 // const minRank = JSON.parse("_list.json").length(); // Retrieves the array containing the list, and gets its length, which is the rank of the last level
+const list = fetch("./data/_list.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        console.log(data.length);
+    });
 
 /**
  * Calculate the score awarded when having a certain percentage on a list level
